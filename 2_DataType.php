732baf5c95ac4a2ad8a2dfd0f551f81a8de00177
snoooer <snoooer@outlook.com>
2017-07-08 06:08:22 -0500

    Add files via upload

---
+++ 2_DataType.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: john
+ * Date: 08/07/17
+ * Time: 18:10
+ */
+$x=12345;
+var_dump($x); //返回变量的数据类型和值
+echo "<br>";
+$x= -234;
+var_dump($x);
+echo "<br>";
+$x=0x8C; //十六进制数
+var_dump($x);
+echo "<br>";
+$x=047; //八进制数
+var_dump($x);
+echo "<br>";
+$x=2.4e3;
+var_dump($x);
+echo "<br>";
+$x=8E-4;
+var_dump($x);
+?>
