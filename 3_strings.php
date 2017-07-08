cac81d6d8eb6bc1e1a3250f3e6c5ad6e314d8be9
snoooer <snoooer@outlook.com>
2017-07-08 06:16:46 -0500

    Add files via upload

---
+++ 3_strings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: john
+ * Date: 08/07/17
+ * Time: 19:09
+ */
+$string = "Hello,World!";
+echo strlen($string);
+echo "<br>";
+echo strpos($string,"World");
+echo "<br>";
+echo strpos($string,"ffff");
+?>
