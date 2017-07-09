e5c15a138ee172e0e2dda512ae8836cae698fc72
snoooer <snoooer@outlook.com>
2017-07-09 08:35:15 -0500

    Add files via upload

---
+++ 5_array.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: john
+ * Date: 08/07/17
+ * Time: 23:58
+ */
+$x = array("a"=>"red","b"=>"green");
+var_dump($x);
+echo "<br>";
+echo $x['a'],"<br>";
+$x = array("red","green");
+echo $x[1];
+?>
