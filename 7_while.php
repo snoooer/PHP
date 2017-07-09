5467d811f21daeb2e32f0bab1b25bab5546ea136
snoooer <snoooer@outlook.com>
2017-07-09 08:36:39 -0500

    Add files via upload

---
+++ 7_while.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: john
+ * Date: 09/07/17
+ * Time: 00:34
+ */
+$x = 1;
+while ($x <= 5){
+    echo "The number is:",$x;
+    echo "<br>";
+    $x++;
+}
+
+$n = 1;
+do {
+    echo "The number is :",$n;
+    echo "<br>";
+    $n++;
+} while ($n <= 5);
+
+$x = 6;
+do {
+    echo "The number is :",$x;
+    echo "<br>";
+    $x++;
+} while ($x <= 5);
+?>
